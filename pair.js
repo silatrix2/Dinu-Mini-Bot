ce3120b899efd5e09d87829a95df75055a28ecc9
DINU-FXMD <darkdenuwan5@gmail.com>
2025-08-14 08:44:43 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -498,8 +498,9 @@ function setupCommandHandlers(socket, number) {
                         image: { url: config.BUTTON_IMAGES.ALIVE },
                         caption: formatMessage(title, content, footer),
                         buttons: [
-                            { buttonId: `${config.PREFIX}sssong`, buttonText: { displayText: 'MNU' }, type: 1 },
-                            { buttonId: `.sssong`, buttonText: { displayText: 'PNG' }, type: 1 }
+                            { buttonId: `${config.PREFIX}sssong lelena`, buttonText: { displayText: 'MNU' }, type: 1 },
+                            { buttonId: `${config.PREFIX}sssong + lelena`, buttonText: { displayText: 'MNU' }, type: 1 },
+                            { buttonId: `.sssong+lelena`, buttonText: { displayText: 'PNG' }, type: 1 }
                         ],
                         quoted: msg
                     });
